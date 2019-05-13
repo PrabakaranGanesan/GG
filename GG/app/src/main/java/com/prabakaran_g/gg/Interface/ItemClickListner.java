aeff6f496a981bb2a1135efecfbc90aa450970e7
praba <prabakaran@prabas-MacBook-Air.local>
2019-05-13 14:41:56 +0530

    done

---
+++ GG/app/src/main/java/com/prabakaran_g/gg/Interface/ItemClickListner.java
@@ -0,0 +1,10 @@
+package com.prabakaran_g.gg.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner
+{
+
+    void onClick(View view, int position, boolean isLongClick);
+
+}
---
+++ GG/app/src/main/java/com/prabakaran_g/gg/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.prabakaran_g.gg.Prevalent;
+
+import com.prabakaran_g.gg.Model.Users;
+
+public class Prevalent
+{
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
